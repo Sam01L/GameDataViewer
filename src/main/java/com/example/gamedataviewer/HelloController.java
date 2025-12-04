29d548f81e8977560e161a495bf1ef968c73121f
sam <zliu26@scienceleadership.org>
2025-12-04 09:12:27 -0500

    1a

--- src/main/java/com/example/gamedataviewer/HelloController.java
+++ src/main/java/com/example/gamedataviewer/HelloController.java
@@ -2,13 +2,23 @@ package com.example.gamedataviewer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public TextField gameTitle;
+    public TextField gameType;
+    public TextField gameRank;
+    public TextArea toStringText;
+    public ListView<VideoGames> allGamesListView;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() throws Exception{
+        GameRating.readGameRatingData();
+        GameCopiesSold.readGameCopiesData();
     }
-}
+    }
+
+
+
+
