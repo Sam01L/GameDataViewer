2003a63cac1402eafe1e7b9c58c303918ad68fa5
sam <zliu26@scienceleadership.org>
2025-12-04 09:22:55 -0500

    1a

--- src/main/java/com/example/gamedataviewer/HelloController.java
+++ src/main/java/com/example/gamedataviewer/HelloController.java
@@ -1,21 +1,34 @@
 package com.example.gamedataviewer;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class HelloController {
     public TextField gameTitle;
     public TextField gameType;
     public TextField gameRank;
     public TextArea toStringText;
-    public ListView<VideoGames> allGamesListView;
+    public TableView<VideoGames> allGamesListView;
 
     public void initialize() throws Exception{
         GameRating.readGameRatingData();
         GameCopiesSold.readGameCopiesData();
+
+        for (VideoGames eachGame : VideoGames.getAllVideoGames()) {
+            allGamesListView.getItems().add(eachGame);
+        }
+
+        allGamesListView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> {
+
+                    if (newValue != null) {
+                        gameRank.setText(String.valueOf(newValue.getRank()));
+                        gameTitle.setText(newValue.getTitle());
+                    } else {
+                        gameRank.setText("");
+                        gameTitle.setText("");
+                    }
+                });
     }
     }
 
